66e145fcd832dc90477b34067d8165c8dba765da
Hardyguardy <fighter2007max@gmail.com>
2024-02-26 18:21:14 +0100

    fix: Breadcrum update

--- src/Components/Breadcrums/Breadcrum.jsx
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -4,7 +4,13 @@ import arrow_icon from "../Assets/breadcrum_arrow.png";
 
 const Breadcrum = (props) => {
   const { product } = props;
-  return <div className="breadcrum">HOME</div>;
+  return (
+    <div className="breadcrum">
+      HOME <img src={arrow_icon} alt="" />
+      SHOP <img src={arrow_icon} alt="" /> {product.category}{" "}
+      <img src="{arrow_icon}" alt="" /> {product.name}
+    </div>
+  );
 };
 
 export default Breadcrum;
