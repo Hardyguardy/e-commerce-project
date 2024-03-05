a24f626ba89636feb0c8d63ec723ebb45e817c22
Hardyguardy <fighter2007max@gmail.com>
2024-03-05 22:00:08 +0100

    feat: add ProductDisplay

---
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ProductDisplay.css";
+
+const ProductDisplay = () => {
+  return (
+    <div className="product-display">
+      <div className="productdisplay-left">
+        <div className="productdisplay-img-list"></div>
+      </div>
+      <div className="productdisplay-right"></div>
+    </div>
+  );
+};
+
+export default ProductDisplay;
