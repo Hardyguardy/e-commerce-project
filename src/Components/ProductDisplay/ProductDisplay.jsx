e628e56840e74353a607b60f5b4298058cd26ec8
Hardyguardy <fighter2007max@gmail.com>
2024-03-07 21:44:48 +0100

    fix: Update ProductDisplay

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./ProductDisplay.css";
+import star_icon from "../Assets/star_icon.png";
+import star_dull_icon from "../Assets/star_dull_icon.png";
 
 const ProductDisplay = (props) => {
   const { product } = props;
@@ -16,7 +18,16 @@ const ProductDisplay = (props) => {
           <img className="productdisplay-main-img" src={product.image} alt="" />
         </div>
       </div>
-      <div className="productdisplay-right"></div>
+      <div className="productdisplay-right">
+        <h1>{product.name}</h1>
+        <div className="productdisplay-right-star">
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_dull_icon} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
